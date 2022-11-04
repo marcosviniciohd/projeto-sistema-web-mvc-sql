0071e6b292a2ebb12044d9c4fa295a3c50950339
Marcos <marcosviniciohd@hotmail.com>
2022-11-04 20:01:35 -0300

    Projeto condominio criado

---
+++ 2022-11-04-projeto-condominio/condominio/src/main/java/br/edu/iftm/marcos/vinicio/condominio/CondominioApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.marcos.vinicio.condominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CondominioApplication.class, args);
+    }
+
+}
---
+++ 2022-11-04-projeto-condominio/condominio/src/test/java/br/edu/iftm/marcos/vinicio/condominio/CondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.marcos.vinicio.condominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondominioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
